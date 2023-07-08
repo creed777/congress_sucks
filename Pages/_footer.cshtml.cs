0b137435d3c63921fe4ae00b00c4e5cc4138c69f
creed777 <usmc6072@zoho.com>
2023-07-08 15:12:12 -0500

    Add project files.

---
+++ Pages/_footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace congress_sucks.Pages
+{
+    public class _footerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
